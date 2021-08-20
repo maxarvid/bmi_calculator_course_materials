43c3576a1c3bb084c3de9f3ea460a62fd274cb98
maxarvid <max.arvid.anderson@gmail.com>
2021-08-20 11:57:19 +0200

    add tests for input fields for height and weight

--- cypress/integration/userCanTestTheirBMI.feature.js
+++ cypress/integration/userCanTestTheirBMI.feature.js
@@ -2,5 +2,7 @@ describe("User can check their BMI", () => {
   it("user can enter weight and height", () => {
     cy.visit("http://localhost:3001");
     cy.contains("BMI Calculator");
+    cy.get("#weight").type("90");
+    cy.get("#height").type("186");
   });
 });
